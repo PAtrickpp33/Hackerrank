730b7fc7fbe35b380e3b38eed4416eaf8b51adc0
PAtrickpp33 <89336859+PAtrickpp33@users.noreply.github.com>
2022-07-17 02:24:15 +0800

    week1

---
+++ week1/Breaking the Records.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    # Write your code here
+    result = [0, 0]
+    
+    for i in range(1,len(scores)):
+        result[1] += scores[i]<min(scores[:i])
+        result[0] += scores[i]>max(scores[:i])
+    
+    return result
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
