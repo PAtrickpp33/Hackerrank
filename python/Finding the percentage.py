bbf681557bb2763ca257ea53cf8556eaa62030bd
PAtrickpp33 <89336859+PAtrickpp33@users.noreply.github.com>
2022-07-18 01:25:04 +0800

    python

---
+++ python/Finding the percentage.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    a,b,c = student_marks[query_name]
+
+    print('%.2f'%((a+b+c)/3))
